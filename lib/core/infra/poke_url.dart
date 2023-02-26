2518c77cb55f3afc4435259470fd7be447114eed
Gabriel Costa <jgccosta21@gmail.com>
2023-02-26 12:53:39 -0300

    chore: add new file of defaults urls

--- lib/core/infra/poke_url.dart
+++ lib/core/infra/poke_url.dart
@@ -1,5 +1,6 @@
 class PokeURL {
-  static const baseURL = 'https://pokeapi.co/api/v2/';
+  static const baseURL = 'https://pokeapi.co/api/v2';
+  static const pokeDetails = '$baseURL/pokemon';
   static const baseImageURL =
       'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/';
 }
